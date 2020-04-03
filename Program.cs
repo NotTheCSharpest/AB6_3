28609317fe8bfa670ff25f0edd34f7802b78cd0c
Soshi <soshi.mimicry@gmail.com>
2020-04-03 16:45:39 +0200

    fertig gemacht und notiert

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AB_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++){ Console.WriteLine(i);};
+            /* Zaehlt
+             * 1
+             * 2
+             * 3
+             */ 
+            for (int i = 1; i <= 3; Console.WriteLine(++i)) {};*/
+            /* Zaehlt
+             * 2
+             * 3
+             * 4
+             */
+             for (int i = 1; ; Console.WriteLine(i++)) { };
+            /* Zaehlt von 1 bis infinity /*
+             
+            /* Der letzte ist unerreichbar 
+             * for (; ; ) { };
+             */
+        }
+    }
+}
